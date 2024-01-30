fad32018f2d6a6e94b748fa0c501ffe454595969
Nazakin <134816178+Nazakin@users.noreply.github.com>
2024-01-30 15:26:03 +0100

    ...

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,10 +1,13 @@
 import React, { Suspense } from 'react';
 import { Route, Routes } from 'react-router-dom';
 
-const Contacts = React.lazy(() => import('../pages/Contacts'));
-const RegisterForm = React.lazy(() => import('../pages/Register'));
-const LoginForm = React.lazy(() => import('../pages/Login'));
-
+const Contacts = React.lazy(() =>
+  import('../pages/Contacts').then(module => ({ default: module.Contacts })));
+  const RegisterForm = React.lazy(() =>
+  import('../pages/Register').then(module => ({ default: module.RegisterForm }))
+);const LoginForm = React.lazy(() =>
+import('../pages/Login').then(module => ({ default: module.LoginForm }))
+);
 export const App = () => {
   return (
     <Suspense fallback={<div>Loading...</div>}>
